374a5c9dc25d9dcb52e5f2f37c4eac3f5b8cd4a7
jyong <wjy9517@163.com>
2023-03-18 08:58:59 +0800

    add flink code

---
+++ Jyong-flink/src/main/java/com/jyong/flink/entity/Event.java
@@ -0,0 +1,54 @@
+package com.jyong.flink.entity;
+
+/**
+ * @author: jyong
+ * @description 数据事件流
+ * @date: 2023/3/17 21:00
+ */
+public class Event {
+
+    private String user;
+
+    private String url;
+
+    private Long timestamp;
+
+    public Event(String user, String url, Long timestamp) {
+        this.user = user;
+        this.url = url;
+        this.timestamp = timestamp;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    @Override
+    public String toString() {
+        return "Event{" +
+                "user='" + user + '\'' +
+                ", url='" + url + '\'' +
+                ", timestamp=" + timestamp +
+                '}';
+    }
+}
