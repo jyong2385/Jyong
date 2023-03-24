ffa89af58b8006b3aab5f1348f92ee8808a17301
jyong <wjy9517@163.com>
2023-03-24 21:40:29 +0800

    flink转换算子

---
+++ Jyong-spring/src/main/java/com/jyong/spring/jyongspring/JyongSpringApplication.java
@@ -0,0 +1,13 @@
+package com.jyong.spring.jyongspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JyongSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JyongSpringApplication.class, args);
+    }
+
+}
---
+++ Jyong-spring/src/test/java/com/jyong/spring/jyongspring/JyongSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jyong.spring.jyongspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JyongSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
