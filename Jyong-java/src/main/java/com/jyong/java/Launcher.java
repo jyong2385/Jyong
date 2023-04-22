20e7d2c55d4859f704c583de0a6c40c17ffdb5f0
jyong <wjy9517@163.com>
2023-04-22 15:35:55 +0800

    update

--- Jyong-java/src/main/java/com/jyong/java/Launcher.java
+++ Jyong-java/src/main/java/com/jyong/java/Launcher.java
@@ -1,21 +1,8 @@
 package com.jyong.java;
 
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.TypeReference;
-
-import java.text.ParseException;
-import java.util.List;
-
 public class Launcher {
-    public static void main(String[] args) throws ParseException {
-
-
-        String str = "[1,2]";
-        List<Long> list = JSON.parseObject(str, new TypeReference<List<Long>>() {
-        });
-        System.out.println(list);
-
-
+    public static void main(String[] args) {
+        System.out.println("running ...");
     }
 }
