bf2d2f2994fad8fd08cf91b047b29e3d888a4c5c
jyong <wjy9517@163.com>
2023-06-23 17:39:46 +0800

    20230623 flink cep风控

--- UserBehaviorAnalysis/HotItemsAnalysis/src/main/java/com/jyong/hotitems_analysis/process/AppMarketingStatistics.java
+++ UserBehaviorAnalysis/HotItemsAnalysis/src/main/java/com/jyong/hotitems_analysis/process/AppMarketingStatistics.java
@@ -5,12 +5,13 @@ import com.jyong.hotitems_analysis.beans.MarketingUserBehavior;
 import org.apache.flink.api.common.eventtime.SerializableTimestampAssigner;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.functions.AggregateFunction;
-import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.java.tuple.Tuple;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.source.SourceFunction;
-import org.apache.flink.streaming.api.functions.windowing.ProcessWindowFunction;
+import org.apache.flink.streaming.api.functions.windowing.WindowFunction;
 import org.apache.flink.streaming.api.windowing.assigners.SlidingEventTimeWindows;
 import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
@@ -25,11 +26,12 @@ import java.util.concurrent.TimeUnit;
 
 /**
  * @Author jyong
- * @Date 2023/6/14 21:38
- * @desc app市场推广统计 分渠道统计
+ * @Date 2023/6/22 21:05
+ * @desc app市场推广统计-统计总量值
  */
 
-public class AppMarketingByChannel {
+public class AppMarketingStatistics {
+
 
     public static void main(String[] args) throws Exception {
 
@@ -47,49 +49,46 @@ public class AppMarketingByChannel {
                                 return marketingUserBehavior.getTimestamp();
                             }
                         }));
+        dataSource.print("source->");
 
-        //3 分渠道开窗统计
-        dataSource.keyBy(new KeySelector<MarketingUserBehavior, Tuple2<String, String>>() {
+        //3 分渠道开窗统计总量
+        dataSource.map(new MapFunction<MarketingUserBehavior, Tuple2<String,Long>>() {
                     @Override
-                    public Tuple2<String, String> getKey(MarketingUserBehavior marketingUserBehavior) throws Exception {
-                        return Tuple2.of(marketingUserBehavior.getChannel(), marketingUserBehavior.getBehavior());
+                    public Tuple2<String, Long> map(MarketingUserBehavior marketingUserBehavior) throws Exception {
+                        return Tuple2.of("total",1L);
                     }
-                })
+                }).keyBy(0)
                 .window(SlidingEventTimeWindows.of(Time.hours(1), Time.seconds(5)))
-                .aggregate(new MarketCountAgg(), new MarketCOuntResult()).print();
+                .aggregate(new AppMarketingStatisticsAgg(),new AppMarketingStatisticsResult())
+                .print("result=>")
+        ;
 
         env.execute();
     }
 
 
     //实现自定义的全窗口函数
-    private static class MarketCOuntResult extends ProcessWindowFunction<Long, ChannelPromotionCount, Tuple2<String, String>, TimeWindow> {
-
-
+    private static class AppMarketingStatisticsResult implements WindowFunction<Long,ChannelPromotionCount, Tuple,TimeWindow>{
         @Override
-        public void process(Tuple2<String, String> key, ProcessWindowFunction<Long, ChannelPromotionCount, Tuple2<String, String>, TimeWindow>.Context context, Iterable<Long> elements, Collector<ChannelPromotionCount> out) throws Exception {
-
-
-            String channel = key.f0;
-            String behavior = key.f1;
-            long windowEnd = context.window().getEnd();
-            String windowend = new Timestamp(windowEnd).toString();
-            Long count = elements.iterator().next();
-            out.collect(new ChannelPromotionCount(channel,behavior,windowend,count));
+        public void apply(Tuple s, TimeWindow window, Iterable<Long> input, Collector<ChannelPromotionCount> out) throws Exception {
+            String windowEnd = new Timestamp(window.getEnd()).toString();
+            Long count = input.iterator().next();
+            out.collect(new ChannelPromotionCount("total","total",windowEnd,count));
         }
     }
 
 
     //自定义实现聚合函数
-    private static class MarketCountAgg implements AggregateFunction<MarketingUserBehavior, Long, Long> {
+    private static class AppMarketingStatisticsAgg implements AggregateFunction<Tuple2<String,Long>, Long, Long> {
+
         @Override
         public Long createAccumulator() {
             return 0L;
         }
 
         @Override
-        public Long add(MarketingUserBehavior marketingUserBehavior, Long accumulator) {
-            return accumulator + 1;
+        public Long add(Tuple2<String, Long> value, Long accumulator) {
+            return accumulator+1;
         }
 
         @Override
@@ -99,7 +98,7 @@ public class AppMarketingByChannel {
 
         @Override
         public Long merge(Long accumulator1, Long accumulator2) {
-            return accumulator1 + accumulator2;
+            return accumulator1+accumulator2;
         }
     }
 
@@ -141,5 +140,8 @@ public class AppMarketingByChannel {
             running = false;
         }
     }
+
+
+
 
 }
