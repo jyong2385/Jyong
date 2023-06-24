514c7a435c23cee973c6a623017c983f829f3183
jyong <wjy9517@163.com>
2023-06-24 20:56:22 +0800

    20230624 flink cep风控

--- UserBehaviorAnalysis/HotItemsAnalysis/src/main/java/com/jyong/hotitems_analysis/process/LoginFailWarnCep.java
+++ UserBehaviorAnalysis/HotItemsAnalysis/src/main/java/com/jyong/hotitems_analysis/process/LoginFailWarnCep.java
@@ -30,10 +30,9 @@ import java.util.Map;
  * @Author jyong
  * @Date 2023/6/23 15:24
  * @desc 连续3ds登陆失败告警 pro
- *  由于之前处理方式有漏洞，2秒内的数据存在并发量很高是出现某一个成功，则会导致无法检测出
- *  当出现2次以上的判断时判断比较麻烦。并且当出现乱序数据时，无法判断连续两次时间的数据
+ * 由于之前处理方式有漏洞，2秒内的数据存在并发量很高是出现某一个成功，则会导致无法检测出
+ * 当出现2次以上的判断时判断比较麻烦。并且当出现乱序数据时，无法判断连续两次时间的数据
  * 对上上述特殊情况，则需求flink提供的负责事件时间处理方式：CEP
- *
  */
 
 public class LoginFailWarnCep {
@@ -70,26 +69,54 @@ public class LoginFailWarnCep {
 
 
         //2。定义cep pattern
-        Pattern<UserLoginInfo, UserLoginInfo> loginFailPattern = Pattern.<UserLoginInfo>begin("first").where(new SimpleCondition<UserLoginInfo>() {
+        Pattern<UserLoginInfo, UserLoginInfo> loginFailPattern0 = Pattern.<UserLoginInfo>begin("first").where(new SimpleCondition<UserLoginInfo>() {
                     @Override
                     public boolean filter(UserLoginInfo userLoginInfo) throws Exception {
                         return "fail".equalsIgnoreCase(userLoginInfo.getStatus());
                     }
-                }).times(3)
-                .within(Time.seconds(3));
+                }).next("second")
+                .where(new SimpleCondition<UserLoginInfo>() {
+                    @Override
+                    public boolean filter(UserLoginInfo userLoginInfo) throws Exception {
+                        return "fail".equalsIgnoreCase(userLoginInfo.getStatus());
+                    }
+                })
+                .next("third")
+                .where(new SimpleCondition<UserLoginInfo>() {
+                    @Override
+                    public boolean filter(UserLoginInfo userLoginInfo) throws Exception {
+                        return "fail".equalsIgnoreCase(userLoginInfo.getStatus());
+                    }
+                })
+                .within(Time.seconds(5));
 
+
+        //优化pattern
+        Pattern<UserLoginInfo, UserLoginInfo> loginFailPattern1 = Pattern.<UserLoginInfo>begin("failEvent").where(new SimpleCondition<UserLoginInfo>() {
+                    @Override
+                    public boolean filter(UserLoginInfo userLoginInfo) throws Exception {
+                        return "fail".equalsIgnoreCase(userLoginInfo.getStatus());
+                    }
+                }).times(3).consecutive()  //consecutive严格近邻
+                .within(Time.seconds(5));
 
 
         //3.利用pattern处理
-        SingleOutputStreamOperator<UserLoginResult> result = CEP.pattern(dataSource, loginFailPattern)
+        SingleOutputStreamOperator<UserLoginResult> result = CEP.pattern(dataSource, loginFailPattern1)
                 .select(new PatternSelectFunction<UserLoginInfo, UserLoginResult>() {
                     @Override
                     public UserLoginResult select(Map<String, List<UserLoginInfo>> map) throws Exception {
                         //获取第2次失败的信息
-                        UserLoginInfo first = map.get("first").get(0);
-                        UserLoginInfo third = map.get("first").get(2);
 
-                        return new UserLoginResult(first.getUserId(),first.getTimestamp(),third.getTimestamp(),3);
+                        //pattern1
+//                        UserLoginInfo first = map.get("first").get(0);
+//                        UserLoginInfo second = map.get("second").get(0);
+
+                        //pattern2
+                        UserLoginInfo first0 = map.get("failEvent").get(0);
+                        UserLoginInfo third3 = map.get("failEvent").get(2);
+
+                        return new UserLoginResult(first0.getUserId(), first0.getTimestamp(), third3.getTimestamp(), 3);
                     }
                 });
         result.print();
